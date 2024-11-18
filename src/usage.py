bb4bc82264904c768426120d780aeb54a07b629c
Dave Budd <david.budd@ovo.com>
2024-11-18 15:36:36 +0000

    Usage and Readme - all the files for all the categories up to 10

--- src/usage.py
+++ src/usage.py
@@ -51,14 +51,16 @@ os.environ["POLYOMINO_DATA_FOLDER"] = "data"
 #         PolyShape.draw(p)
 #         print()
 
-# Plot
-# HexagonPoly.plot("327680-32768-65792-133632-65808-131616-262464-164352-21504")
-# HexagonPoly.plot("262144-532480-1130496-655392-1048896-2097696-1053696-663552-81920")
-# HexagonPoly.plot("10")
-# HexagonPoly.plot("84-10-4")
-SquarePoly.plot("112-28-7-44-56")
+# Plotting - Opens a window if to_file=False
+# HexagonPoly.plot(
+#     "327680-32768-65792-133632-65808-131616-262464-164352-21504", to_file=False
+# )
+# HexagonPoly.plot(
+#     "262144-532480-1130496-655392-1048896-2097696-1053696-663552-81920", to_file=False
+# )
+# SquarePoly.plot("112-28-7-44-56", to_file=False)
 
-
+# Proving the result for the largest square plane when k<=3
 # create_data(SquarePoly, Plane, 1, 16, 3)
 # output_table(SquarePoly, Plane, 16, k_limit=3)
 # result = ", ".join(
@@ -68,3 +70,21 @@ SquarePoly.plot("112-28-7-44-56")
 
 # expected = "1, 1, 2, 4, 9, 18, 37, 62, 86, 78, 61, 34, 14, 4, 1, 0"
 # assert result == expected
+
+# create all the files for all the categories up to 10
+max_n = 10
+for n in range(1, max_n + 1):
+    for k in range(1, n + 1):
+        create_ancestors_nk(SquarePoly, Lattice, n, k)
+        create_ancestors_nk(SquarePoly, Plane, n, k)
+        create_ancestors_nk(HexagonPoly, Lattice, n, k)
+        create_ancestors_nk(HexagonPoly, Plane, n, k)
+
+print("A377941")
+output_table(SquarePoly, Lattice, 10)
+print("A377942")
+output_table(SquarePoly, Plane, 10)
+print("A378014")
+output_table(HexagonPoly, Lattice, 10)
+print("A378015")
+output_table(HexagonPoly, Plane, 10)
